2608d2f16ba45d6290f994ce1b2f81728a4f71da
Fred Morrison <frederick.morrison@outlook.com>
2024-11-25 12:40:20 -0700

    move patient.json to separate subdirectory

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+//
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.ZonedDateTime;
+import java.util.stream.Stream;
+//
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
@@ -21,17 +28,38 @@ public class Main {
      */
     private static void verifyPOJOtoJSON() {
         System.out.format("%n%s%n", getMethodName(1));
-        final String filename = "patient.json";
+
+        Path currentDir = Paths.get("").toAbsolutePath();
+        System.out.format("Current working directory: %s%n",currentDir);
+
+        final String targetFileName = "patient.json";
+        final String basePath = ".";
+        final Path baseDir = Paths.get(".");        
+
+        Path foundFile = null;
+        try {
+            foundFile = findFirstOccurrence(baseDir, targetFileName);
+            if (foundFile != null) { // .fs.defaultDirectory or .path
+                // System.out.println("File found: " + foundFile.toAbsolutePath());
+                Path realPath = foundFile.toRealPath();
+                System.out.format("Real path: %s%n",realPath);
+            } else {
+                System.out.println("File not found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Gson g = getDefaultGson();
         // create a Patient instance from an external JSON text file
         try {
-            File f = new File(filename);
+            File f = new File(foundFile.toUri());
             if (f.exists()) {
                 FileReader fr = new FileReader(f);
                 Patient p = g.fromJson(fr, Patient.class);
                 System.out.println(p);
             } else {
-                String msg = String.format("File %s not found! Expected at %s.%n", filename, f.getAbsolutePath());
+                String msg = String.format("File %s not found! Expected at %s.%n", targetFileName, f.getAbsolutePath());
                 System.err.println(msg);
                 throw new FileNotFoundException(msg);
             }
@@ -41,6 +69,29 @@ public class Main {
         printSeparator();
     }
 
+    
+
+    public static Path findFirstOccurrence(Path baseDir, String targetFileName) throws IOException {
+        final Path[] result = {null}; // To store the result as a shared variable
+
+        Files.walkFileTree(baseDir, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                if (file.getFileName().toString().equals(targetFileName)) {
+                    result[0] = file; // Store the found file
+                    return FileVisitResult.TERMINATE; // Stop traversal once found
+                }
+                return FileVisitResult.CONTINUE; // Continue if not found
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE; // Skip unreadable files
+            }
+        });
+
+        return result[0]; // Return the found file, or null if not found
+    }
 
     private static void printSeparator() {
         StringBuilder sb = new StringBuilder();
